1a6e0bc5b529a3d4e118fa6ed2804a607e5973ac
Bhargavan Marepalli <bhargavan.marepalli@gmail.com>
2016-03-22 20:46:42 -0400

    First Commit

---
+++ src/DemoFilter.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class DemoFilter
+ */
+@WebFilter("/DemoFilter")
+public class DemoFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public DemoFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		PrintWriter out = response.getWriter();
+		out.println("This line is added by filter before processing request");
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+		
+		out.println("This line is added by filter after processing request");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
